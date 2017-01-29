b68010f9409aaeb454b6429b6aef263405316495
bhushannatu <bhushannatu01@gmail.com>
2017-01-29 15:44:38 +0530

    typescriptsdemo

---
+++ TypeScript/funda.js
@@ -0,0 +1,10 @@
+var myNum = 'android';
+console.log("Os Name is " + myNum);
+function whatIsOs() {
+    return myNum;
+}
+function thisisVersion(ver1) {
+    console.log("Os vrsion is " + ver1);
+}
+whatIsOs();
+thisisVersion(2);
---
+++ TypeScript/funda.ts
@@ -0,0 +1,16 @@
+let myNum:string ='android';
+console.log(`Os Name is ${myNum}`)
+
+function whatIsOs():string{
+
+    return myNum;
+}
+
+function thisisVersion(ver1:number){
+    console.log(`Os vrsion is ${ver1}`)
+}
+
+
+
+ whatIsOs();
+thisisVersion(2);
